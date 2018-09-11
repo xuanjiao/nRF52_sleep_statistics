2d7688ac23ee18203f9f37dd645fb4e64707386b
Jurica Rešetar <jurica_resetar@yahoo.com>
2018-09-11 14:56:13 +0200

    Init commit

---
+++ aconnoConfig.h
@@ -0,0 +1,10 @@
+/**
+ * Made by Jurica @ aconno
+ */
+
+#ifndef __ACONNO_CONFIG_H__
+#define __ACONNO_CONFIG_H__
+
+
+
+#endif //__ACONNO_CONFIG_H__
---
+++ main.cpp
@@ -0,0 +1,17 @@
+/**
+ * Made by Jurica @ aconno
+ * Simple program to demonstrate how to use mbed-os statistics for power
+ * consumption tracking
+ */
+
+#include "mbed.h"
+
+int main()
+{
+	while(1)
+	{
+		printf("In the main.");
+		printf("O da.\n");
+		wait_ms(234);
+	}
+}

bb4010ca5383e6f417436628009b358ed1a013e0
jiao <xz.aconno@gmail.com>
2018-10-19 14:55:10 +0200

    add some discribtion for sleep manager and deep sleep lock(mbed os 5.10)

--- main.cpp
+++ main.cpp
@@ -11,50 +11,128 @@
 #error [NOT_SUPPORTED] test not supported
 #endif
 
+uint64_t prev_idle_time = 0;
+uint64_t prev_uptime = 0;
+uint64_t prev_sleep_time = 0;
+uint64_t prev_deep_sleep_time = 0;
+
+#define LOOP_TIME_MS 3000
 DigitalOut led1(p22);
 
-uint64_t prev_idle_time = 0;
 int32_t wait_time = 1000;
 
 void busy_thread()
 {
-	led1 = !led1;
+    
+    printf("lock deep sleep\r\n");
     wait_ms(wait_time);
- 	for(int i=~0; i>0; i--)
+    for(int i=~0; i>0; i--)
 	{
-		// Just loop for some time
+	// Just loop for some time
 		wait_ms(1);
 	}
 }
 
+void busy_thread_20_s(){
+	  
+	{
+	    	DeepSleepLock lock;     	 	
+	 	printf("\ndeep sleep lock object is created\r\n");
+		
+		// Code in this block will run with the deep sleep mode locked	
+	  	 
+		ThisThread::sleep_for(20000);
+	}
+	printf("deep lock object is restored\r\n");
+	ThisThread::sleep_for(20000);
+	return;
+}
+
 void print_stats()
 {
+    led1 = !led1;
     mbed_stats_cpu_t stats;
     mbed_stats_cpu_get(&stats);
 
-    printf("%-20lld", stats.uptime/1000);
-    printf("%-20lld", stats.idle_time/1000);
-    printf("%-20lld", stats.sleep_time/1000);
-    printf("%-20lld\r\n", stats.deep_sleep_time/1000);
-}
+    uint64_t diff_idle_time,
+             diff_uptime,
+             diff_sleep_time,
+             diff_deep_sleep_time,
+             current_uptime,
+             current_idle_time,
+             current_sleep_time,
+             current_deep_sleep_time;
+    uint8_t idle_pec,
+            up_pec,
+            sleep_pec,
+            deep_sleep_pec;
+
+    current_uptime              = stats.uptime/1000;
+    current_idle_time           = stats.idle_time/1000;
+    current_sleep_time          = stats.sleep_time/1000;
+    current_deep_sleep_time     = stats.deep_sleep_time/1000;
+
+    diff_uptime         = current_uptime - prev_uptime;
+    diff_idle_time      = current_idle_time - prev_idle_time;
+    diff_sleep_time     = current_sleep_time - prev_sleep_time;
+    diff_deep_sleep_time        = current_deep_sleep_time - prev_deep_sleep_time;
+
+    up_pec = diff_uptime*100/diff_uptime;
+    idle_pec = diff_idle_time*100/diff_uptime;
+    sleep_pec = diff_sleep_time*100/diff_uptime;
+    deep_sleep_pec = diff_deep_sleep_time*100/diff_uptime;
+
+    printf("%-20lld",current_uptime);
+    printf("%-20lld",current_idle_time);
+    printf("%-20lld",current_sleep_time);
+    printf("%-20lld",current_deep_sleep_time);
+
+    printf("| %-20lld",diff_uptime);
+    printf("%-20lld",diff_idle_time);
+    printf("%-20lld",diff_sleep_time);
+    printf("%-20lld",diff_deep_sleep_time);
+
+    printf("| %d %d %d %d | Useage: %d\r\n",
+                    up_pec,
+                    idle_pec,
+                    sleep_pec,
+                    deep_sleep_pec,
+                    100-idle_pec);
+
+    prev_idle_time = current_idle_time;
+    prev_uptime = current_uptime;
+    prev_sleep_time = current_sleep_time;
+    prev_deep_sleep_time = current_deep_sleep_time;
+ }
 
 int main()
 {
-    // Request the shared queue
+  
+	// Request the shared queue
     EventQueue *stats_queue = mbed_event_queue();
     Thread *thread;
     int id;
-
     id = stats_queue->call_every(SAMPLE_TIME_MS, print_stats);
-    printf("%-20s%-20s%-20s%-20s\r\n", "Uptime[ms]", "Idle Time[ms]",
+    printf("%-20s%-20s%-20s%-20s\n", "Uptime[ms]", "Idle Time[ms]",
 		"Sleep time[ms]", "DeepSleep time[ms]");
-
+     printf("%-20s%-20s%-20s%-20s\n", "Period Uptime[ms]", "Period Idle Time[ms]",  
+		"Period Sleep time[ms]", "Period DeepSleep time[ms]");
+     printf("%-20s%-20s%-20s%-20s\n", "Uptime percentage[%]", "Idle percentage[%]",  
+		"Slieep percentage[%]", "DeepSleep percentage[%]");
+    
     thread = new Thread(osPriorityNormal, 1024);
-    thread->start(busy_thread);
+    
+     thread->start(busy_thread_20_s);
 
-	stats_queue->dispatch_forever();
+    
 
-	printf("Test is over.\n");
+    // Deep sleep allow
+    //printf("allow deep sleep\r\n");
+    //sleep_manager_can_deep_sleep();
+    
+    stats_queue->dispatch_forever();
+
+    printf("Test is over.\n");
     thread->terminate();
     stats_queue->cancel(id);
     return 0;
--- readMe.md
+++ readMe.md
@@ -17,3 +17,60 @@ Program prints statistics on stdio every SAMPLE_TIME_MS with the following data:
 >**Idle time** - Time spent in idle thread (Idle time is sleep time + deep sleep time)
 
 >**Awake time** - Time spent in users thread (outside idle and sleep modes: active time - idle time)
+
+
+
+## New mbed OS features (mbed OS 5.10):
+
+
+
+### Sleep manager
+
+##### What is sleep manager
+
+Sleep manager is the part of the system that decides which sleep mode we pick. To make sleep manager pick sleep mode, we can hold deep sleep lock.
+
+
+
+![](D:\aconnoMbedStatistics\Image\sleep_manager.PNG)
+
+
+
+### Deep sleep lock
+
+if your application requires high frequency requires ticker or short wake up time, it is better to hold deep sleep lock. It prevents deep sleep.
+
+
+
+#### Some comparation on before and after holding a deep sleep lock
+
+Program prints statistics on RTT Viewer. It prints Uptime, idle time, sleep time, deep sleep time,CPU usage.
+
+
+
+**Hold a deep sleep lock**
+
+ In a period of 5000us, the processor doesn't go to deep sleep. It sleep for 4998us instead. 
+
+![1539947941438](D:\aconnoMbedStatistics\Image\hold_deep_sleep_lock_time_highlight.png)
+
+**Deep sleep lock is released**
+
+ In a period of 5000us, the processor deep sleep for 4998us 
+
+![1539947490039](D:\aconnoMbedStatistics\Image\allow_deep_sleep_time_highlight.png)
+
+
+
+#### How to hold and release a deep sleep lock
+
+- when a DeepSleepLock object is created, its constructor is called, and the lock is held.
+- when the DeepSleepLock object is restored, the lock is relased, and the deep sleep mode will be restored to their previous state
+
+![how_to_lock_deep_sleep](D:\aconnoMbedStatistics\Image\how_to_lock_deep_sleep.PNG)
+
+For more information please refer to 
+
+- [Office Hours video about low power, tickless and sleep](https://www.youtube.com/watch?v=OFfOlBaegdg).
+- DeepSleepLock class reference: https://os.mbed.com/docs/v5.10/apis/deepsleeplock.html#deepsleeplock-class-reference
+
